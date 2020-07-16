4878fe5c6137cf4fc9b45d2ea1b7ce4744bf8ebc
unknown <tommyconner96@gmail.com>
2020-07-16 18:41:32 -0400

    streatch goal!

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -7,17 +7,25 @@ module.exports = {
 
 function success(item) {
   if (item.enhancement < 20) {
+    // if enhancement less than 20, add 1. 
     return { ...item, enhancement: item.enhancement + 1 }
   } else {
+    // otherwise do not.
     return { ...item }
   }
 }
 
 function fail(item) {
   if (item.enhancement < 15) {
+    // if enhancement < 15, we subtract 5 from durability upon fail.
     return { ...item, durability: item.durability - 5 }
   } else if (item.enhancement > 16) {
+    // if enhancement is greater than 16, 
+    // we subtract 1 from enhancement AND 10 from durability upon fail
     return { ...item, enhancement: item.enhancement - 1, durability: item.durability - 10 }
+    // if greater than or equal to 15, subtract 10 from durability.
+    // this one is essentially only called for enhancement of
+    // 15 or 16. if its 17 or greater, the one above will be called.
   } else if (item.enhancement >= 15) {
     return { ...item, durability: item.durability - 10 }
   } else {
@@ -26,9 +34,17 @@ function fail(item) {
 }
 
 function repair(item) {
+  //simply returns the item wityh updated durability of 100.
   return { ...item, durability: 100 }
 }
 
 function get(item) {
-  return { ...item };
+  if (item.enhancement > 0) {
+    // if enhancement is greater then 0, return item with the enhancement formatted properly with
+    // the brackets and plus sign and then the item.name
+    return { ...item, name: `[+` + item.enhancement + `]` + item.name }
+  } else {
+    // otherwise if it is 0 just return item
+    return { ...item }
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,42 +1,20 @@
 const enhancer = require('./enhancer.js')
 // test away!
 
-const testRepair = {
-     name: 'test',
-     enhancement: 13,
-     durability: 69,
-}
-
-const testSuccess = {
-     name: 'player1',
-     enhancement: 16,
-     durability: 50
-}
-
-const testSuccess2 = {
-     name: 'player2',
-     enhancement: 20,
-     durability: 50
-}
-
-const testFail1 = {
-     name: 'player3',
-     enhancement: 14,
-     durability: 60
-}
-
-const testFail2 = {
-     name: 'player4',
-     enhancement: 19,
-     durability: 75
-}
 // REPAIR function testing
 describe('repair', function () {
+     // test data
+     const testRepair = {
+          name: 'test',
+          enhancement: 13,
+          durability: 69,
+     }
      it('should change durability to 100 from whatever it is', function () {
           expect(testRepair.durability).toBe(69)
           expect(enhancer.repair(testRepair)).toBeTruthy()
           expect(enhancer.repair(testRepair)).toEqual({
                ...testRepair,
+               //ensuring that durabililty is changed to 100
                durability: 100,
           })
      })
@@ -44,11 +22,25 @@ describe('repair', function () {
 
 // SUCCESS function testing
 describe('success', function () {
+     // test data
+     const testSuccess = {
+          name: 'player1',
+          enhancement: 16,
+          durability: 50
+     }
+
+     const testSuccess2 = {
+          name: 'player2',
+          enhancement: 20,
+          durability: 50
+     }
+
      it('should add 1 to enhancement if less than 20', function () {
           expect(testSuccess.enhancement).toBe(16)
           expect(enhancer.success(testSuccess))
                .toEqual({
                     ...testSuccess,
+                    // adding 1, making it 17 instead of 16 in this case
                     enhancement: 17,
                })
      })
@@ -57,6 +49,8 @@ describe('success', function () {
           expect(enhancer.success(testSuccess2))
                .toEqual({
                     ...testSuccess2,
+                    // ensures that enhancement stays at 20 and does not 
+                    // get added anything
                     enhancement: 20,
                })
      })
@@ -64,23 +58,43 @@ describe('success', function () {
 
 // FAIL function testing
 describe('fail', function () {
+     // test data
+     const testFail1 = {
+          name: 'player3',
+          enhancement: 14,
+          durability: 60
+     }
+
+     const testFail2 = {
+          name: 'player4',
+          enhancement: 19,
+          durability: 75
+     }
+
+     const testFail3 = {
+          name: 'player1',
+          enhancement: 16,
+          durability: 50
+     }
+
      it('should decrease durability by 5 IF enhancement < 15', function () {
           // this one will decrease durability by 5 because it is less than 15
           expect(testFail1.enhancement).toBe(14)
           expect(enhancer.fail(testFail1))
                .toEqual({
                     ...testFail1,
-                    enhancement: 14,
+                    // checks that it went from 60 to 55
                     durability: 55
                })
      })
 
      it('should decrease durability by 10 IF enhancement >= 15', function () {
           // this one will decrease durability by 10 because it is greater than 15
-          expect(testSuccess.enhancement).toBe(16)
-          expect(enhancer.fail(testSuccess))
+          expect(testFail3.enhancement).toBe(16)
+          expect(enhancer.fail(testFail3))
                .toEqual({
-                    ...testSuccess,
+                    ...testFail3,
+                    // checks that it went from 50 to 40
                     durability: 40
                })
      })
@@ -94,5 +108,37 @@ describe('fail', function () {
                     enhancement: 18,
                     durability: 65
                })
+     })
+})
+
+// STRETCH
+// GET
+
+describe('get', function () {
+     // test data
+     const testGet1 = {
+          name: 'testGet 1',
+          enhancement: 0,
+          durability: 69,
+     }
+
+     const testGet2 = {
+          name: 'test Get 2',
+          enhancement: 10,
+          durability: 72
+     }
+     it('should not change the name if enhancement is 0', function () {
+          expect(testGet1.enhancement).toBe(0)
+          // expect(enhancer.get(testGet1.name)).toEqual(testGet1.name)
+          const getData = enhancer.get(testGet1)
+          expect(getData.name).toBe(testGet1.name)
+
+     })
+     it('should include enhancement level with + sign if greater than 0', function () {
+          expect(testGet2.enhancement).toBe(10)
+          // expect(enhancer.get(testGet2.name)).toBe(`[+` + testGet2.enhancement + `]` + testGet2.name)
+          const getData = enhancer.get(testGet2)
+          expect(getData.name).toBe(`[+` + testGet2.enhancement + `]` + testGet2.name)
+          console.log(getData.name)
      })
 })

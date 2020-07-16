25312a3e8f5a06409ea7f1874e3273d192820887
unknown <tommyconner96@gmail.com>
2020-07-16 09:40:48 -0400

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,27 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 }
+  } else {
+    return { ...item }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 }
+  } else if (item.enhancement > 16) {
+    return { ...item, enhancement: item.enhancement - 1, durability: item.durability - 10 }
+  } else if (item.enhancement >= 15) {
+    return { ...item, durability: item.durability - 10 }
+  } else {
+    return { ...item }
+  }
 }
 
 function repair(item) {
-  return { ...item, durability: 100 };
+  return { ...item, durability: 100 }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,19 +1,97 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require('./enhancer.js')
 // test away!
 
 const testRepair = {
-    name: 'test',
-    enhancement: 13,
-    durability: 69,
+     name: 'test',
+     enhancement: 13,
+     durability: 69,
 }
 
-describe('repair', function(){
-    it('should change durability to 100 from whatever it is', function(){
-         expect(testRepair.durability).toBe(69)
-         expect(enhancer.repair(testRepair)).toBeTruthy()
-         expect(enhancer.repair(testRepair)).toEqual({
-              ...testRepair,
-              durability: 100,
-         })
-    })
+const testSuccess = {
+     name: 'player1',
+     enhancement: 16,
+     durability: 50
+}
+
+const testSuccess2 = {
+     name: 'player2',
+     enhancement: 20,
+     durability: 50
+}
+
+const testFail1 = {
+     name: 'player3',
+     enhancement: 14,
+     durability: 60
+}
+
+const testFail2 = {
+     name: 'player4',
+     enhancement: 19,
+     durability: 75
+}
+// REPAIR function testing
+describe('repair', function () {
+     it('should change durability to 100 from whatever it is', function () {
+          expect(testRepair.durability).toBe(69)
+          expect(enhancer.repair(testRepair)).toBeTruthy()
+          expect(enhancer.repair(testRepair)).toEqual({
+               ...testRepair,
+               durability: 100,
+          })
+     })
+})
+
+// SUCCESS function testing
+describe('success', function () {
+     it('should add 1 to enhancement if less than 20', function () {
+          expect(testSuccess.enhancement).toBe(16)
+          expect(enhancer.success(testSuccess))
+               .toEqual({
+                    ...testSuccess,
+                    enhancement: 17,
+               })
+     })
+     it('should NOT add anything if enhancement is 20', function () {
+          expect(testSuccess2.enhancement).toBe(20)
+          expect(enhancer.success(testSuccess2))
+               .toEqual({
+                    ...testSuccess2,
+                    enhancement: 20,
+               })
+     })
+})
+
+// FAIL function testing
+describe('fail', function () {
+     it('should decrease durability by 5 IF enhancement < 15', function () {
+          // this one will decrease durability by 5 because it is less than 15
+          expect(testFail1.enhancement).toBe(14)
+          expect(enhancer.fail(testFail1))
+               .toEqual({
+                    ...testFail1,
+                    enhancement: 14,
+                    durability: 55
+               })
+     })
+
+     it('should decrease durability by 10 IF enhancement >= 15', function () {
+          // this one will decrease durability by 10 because it is gerater than 15
+          expect(testSuccess.enhancement).toBe(16)
+          expect(enhancer.fail(testSuccess))
+               .toEqual({
+                    ...testSuccess,
+                    durability: 40
+               })
+     })
+     it('should decrease enhancement level by 1 if it is greater than 16', function () {
+          // this one will decrease durability by 10 because it is gerater than 15
+          expect(testFail2.enhancement).toBe(19)
+          expect(enhancer.fail(testFail2))
+               .toEqual({
+                    ...testFail2,
+                    enhancement: 18,
+                    durability: 65
+               })
+     })
 })
